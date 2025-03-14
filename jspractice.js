eb20cfc7caffed4db72bb5959f9f97142e05375d
みつちんゆ〜 <163833605+hanamitsu123@users.noreply.github.com>
2025-03-15 00:29:50 +0900

    Add files via upload
    js学習

---
+++ jspractice.js
@@ -0,0 +1,46 @@
+/** 変数宣言*/
+// var val1 = "var変数";
+// console.log(val1);
+
+// //上書き可能
+// var1 = "var変数を上書き";
+// console.log(val1);
+
+// //var変数は再宣言可能です。
+// var va1 = "var変数を再宣言";
+// console.log(var1);
+
+// let val2 = "let変数";
+// console.log(val2);
+// //letは再宣言不可
+// let val2 = "再宣言";
+
+// const val3 = "const変数";
+// console.log(val3);
+
+// // //上書き不可
+// // val3 = "const変数";
+// // console.log(val3);
+
+// //再宣言不可
+// const val3 = "const変数";
+// console.log(val3);
+
+//const定義オブジェクトはプロパティの変更が可能
+// const val4 = {
+//   name:"じゃけえ",
+//   age:31
+// };
+// console.log(val4);
+// val4.name = "鰹";
+// val4.addres = "kawasaki";
+// console.log(val4);
+
+//const定義の配列はプロパティの変更が可能
+// const val5 = ["dog" , "cat"];
+// console.log(val5);
+
+// val5[0] = "bird";
+// console.log(val5);
+// val5.push("monkey");
+// console.log(val5);
